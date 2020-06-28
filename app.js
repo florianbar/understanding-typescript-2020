e9acdcceeb966c20b3d0332d1b5867b56f99a6df
florianbar <florianbar01@gmail.com>
2020-06-28 15:29:41 +0200

    Added object type

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+var person = {
+    name: "Florian",
+    age: 34
+};
+console.log(person.name);
